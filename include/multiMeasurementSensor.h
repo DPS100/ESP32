549f1769bf181f374cc68f62c44d57d9af798c7f
dgpenastein <dgpenastein@wpi.edu>
2025-04-22 11:45:50 +0200

    Fix build errors

--- include/multiMeasurementSensor.h
+++ include/multiMeasurementSensor.h
@@ -7,6 +7,7 @@
 
 class IMultiMeasurementSensor : public Sensor {
     public:
+        IMultiMeasurementSensor(String description) : Sensor(description, false) {}
         virtual ~IMultiMeasurementSensor() = default;
         virtual void takeMeasurements() = 0;
 
@@ -20,10 +21,21 @@ class IMultiMeasurementSensor : public Sensor {
 };
 
 template <typename T>
+class MultiSensorWrapper : public Sensor {
+    public:
+        MultiSensorWrapper(String description, T* reading) : Sensor(description), reading(reading) {}
+        T* reading;
+        float takeMeasurement() override {
+            return static_cast<float>(*reading);
+        }
+
+};
+
+template <typename T>
 class MultiMeasurementSensor : public IMultiMeasurementSensor {
     public:
 
-        MultiMeasurementSensor(String description, std::vector<String> sensorDescriptions) : Sensor(description, false) {
+        MultiMeasurementSensor(String description, std::vector<String> sensorDescriptions) : IMultiMeasurementSensor(description) {
             numSensors = sensorDescriptions.size();
             readings = std::vector<T>(numSensors);
             sensors = std::vector<Sensor*>(numSensors);
@@ -35,17 +47,6 @@ class MultiMeasurementSensor : public IMultiMeasurementSensor {
 
         void takeMeasurements() override = 0;
         std::vector<T> readings;
-};
-
-template <typename T>
-class MultiSensorWrapper : public Sensor {
-    public:
-        MultiSensorWrapper(String description, T* reading) : Sensor(description), reading(reading) {}
-        T* reading;
-        float takeMeasurement() override {
-            return static_cast<float>(*reading);
-        }
-
 };
 
 
